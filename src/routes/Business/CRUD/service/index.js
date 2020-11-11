c202a0c6221bc6116aed231e1d31441a798c47c8
黃冠霖 <dolphins1123@gmail.com>
2020-11-11 11:20:28 +0800

    webapi  改CALL   LOCAL   CRUD API改CALL   LOCAL   CRUD API

--- src/routes/Business/CRUD/service/index.js
+++ src/routes/Business/CRUD/service/index.js
@@ -10,7 +10,7 @@ export async function getData(payload) {
     payload.pageData.pageSize
 
   console.log('offset====', offset)
-  const _url = `https://localhost:44347/api/Customer/GetList?offset=${offset}&limit=${
+  const _url = `http://127.0.0.1/crud/api/Customer/GetList?offset=${offset}&limit=${
     payload.pageData.pageSize
   }&filters=${JSON.stringify(payload.pageData.filters)}`
   return $$.get(
@@ -32,7 +32,7 @@ export async function getData(payload) {
 export async function Update(payload) {
   const model = payload //JSON.stringify(payload)
 
-  const _url = 'https://localhost:44347/api/Customer/UPDATE'
+  const _url = 'http://127.0.0.1/crud/api/Customer/UPDATE'
   return $$.post(
     _url,
     model,
@@ -53,7 +53,7 @@ export async function Update(payload) {
 export async function Create(payload) {
   const model = payload //JSON.stringify(payload)
 
-  const _url = 'https://localhost:44347/api/Customer/Create'
+  const _url = 'http://127.0.0.1/crud/api/Customer/Create'
   return $$.post(
     _url,
     model,
@@ -80,41 +80,19 @@ export async function Delete(payload) {
 
   const delpar = { idList: delList }
   console.log('del payload =', delpar)
-  const _url = `https://localhost:44347/api/Customer/Delete?idList=${delList}`
+  const _url = `http://127.0.0.1/crud/api/Customer/Delete?idList=${delList}`
 
-  fetch(_url, {})
-    .then((response) => {
-      // 這裡會得到一個 ReadableStream 的物件
-      console.log(response)
-      // 可以透過 blob(), json(), text() 轉成可用的資訊
-      return response
+  return $$.get(
+    _url,
+    {},
+    {
+      //responseType: null,  // 不处理返回结果
+      afterResponse: null, // 不走公共 afterResponse
+    }
+  )
+    .then(function (resp) {
+      console.log(resp)
+      return resp
     })
-    .then((jsonData) => {
-      console.log(jsonData)
-    })
-    .catch((err) => {
-      console.log('錯誤:', err)
-    })
-
-  // return $$.get(
-  //   _url,
-  //   {},
-  //   {
-  //     //responseType: null, // 不处理返回结果
-  //     //afterResponse: null, // 不走公共 afterResponse
-  //   }
-  // )
-  //   .then((resp) => {
-  //     alert('ok1')
-  //     console.log('delete resp.success=', resp.success)
-  //     return resp.success
-  //   })
-  //   .then((resp) => {
-  //     alert('ok2')
-  //     console.log('delete resp.success=', resp.success)
-  //     return resp.success
-  //   })
-  //   .catch((e) => {
-  //     console.log(e)
-  //   })
+    .catch((e) => console.log(e))
 }
